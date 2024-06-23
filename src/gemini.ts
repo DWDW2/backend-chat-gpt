fe799df6f2fcd2c1ad021cf4ff703ed6d5911d21
DWDW2 <daid8002@gmail.com>
2024-06-23 20:48:07 +0500

    he

--- src/gemini.ts
+++ src/gemini.ts
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI= new GoogleGenerativeAI('AIzaSyDBvkSfTeqPqBdT1PfWD_Y2-MIVQMia6tQ');
+const genAI= new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
 
 const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
 
